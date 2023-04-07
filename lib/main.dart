652ceb83d76b465ab887d0ee12c65b623f9de024
jothi.ry14@gmail.com <41383077+jotheeswari@users.noreply.github.com>
2023-04-07 18:14:08 +0530

    Title changed

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("Title"),
       ),
       body: Center(
         child: Column(
